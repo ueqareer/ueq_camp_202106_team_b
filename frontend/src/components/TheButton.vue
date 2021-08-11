87bf57eb1198aa3296115fe5dd937f3dc9f7155d
tayzar-v98 <tayzar@6ck.jp>
2021-08-11 21:28:31 +0900

    Setup frontend

---
+++ frontend/src/components/TheButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="button">
+    <router-link :to=to>{{description}}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheButton",
+  props:['to','description']
+}
+</script>
+
+<style lang="scss" scoped>
+.button{
+  width: 150px;
+  height: 50px;
+  background: rgba(35, 31, 32, 0.76);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  a{
+    text-decoration: none;
+    color : white;
+    font-weight: bold;
+  }
+
+
+}
+</style>
